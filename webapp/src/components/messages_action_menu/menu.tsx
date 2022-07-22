90cf91bd9dba54914a6c052b91ae9e756d14060f
HandsomeChoco <kkb931115@gmail.com>
2022-07-22 20:40:18 +0900

    에잇 귀찮아, 리듀서 전부 다 한번에 작성!

--- webapp/src/components/messages_action_menu/menu.tsx
+++ webapp/src/components/messages_action_menu/menu.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 
+import {useStore} from 'react-redux';
+
 import GoogleIcon from 'components/icon/google_icon';
+import {id} from 'manifest';
 
 function MenuItem() {
+    const store = useStore();
+    console.log(store.getState()[`plugins-${id}`]);
     return (
         <li
             className='MenuItem MenuItem--with-icon'
             role='menuitem'
         >
-            <button className='style--none'>
-                <span className='MenuItem__primary-text'>
-                    <span className='icon'>
-                        <span>
-                            <GoogleIcon/>
+            <a
+                href='https://translate.google.com/?sl=en&tl=ko&text=translate%20something%20&op=translate'
+                target='_blank'
+                rel='noreferrer'
+            >
+                <button className='style--none'>
+                    <span className='MenuItem__primary-text'>
+                        <span className='icon'>
+                            <span>
+                                <GoogleIcon/>
+                            </span>
                         </span>
                     </span>
-                </span>
-                {'구글에서 번역'}
-            </button>
+                    {'구글에서 번역'}
+                </button>
+            </a>
         </li>
     );
 }
--- webapp/src/reducer/reducer.ts
+++ webapp/src/reducer/reducer.ts
@@ -11,6 +11,64 @@ function showModalReducer(state = false, action) {
     }
 }
 
+function srcLangReducer(state = 'set_src_auto', action) {
+    switch (action.type) {
+    case 'set_src_auto':
+        return 'src_auto';
+    case 'set_src_en':
+        return 'src_en';
+    case 'set_src_kr':
+        return 'src_kr';
+    case 'set_src_ru':
+        return 'src_ru';
+    default:
+        return 'src_auto';
+    }
+}
+
+function targetLangReducer(state = 'set_target_kr', action) {
+    switch (action.type) {
+    case 'set_target_auto':
+        return 'target_auto';
+    case 'set_target_en':
+        return 'target_en';
+    case 'set_target_kr':
+        return 'target_kr';
+    case 'set_target_ru':
+        return 'target_ru';
+    default:
+        return 'target_auto';
+    }
+}
+
+function mdFilterReducer(state = 'set_filter_all', action) {
+    switch (action.type) {
+    case 'set_filter_all':
+        return 'filter_all';
+    default:
+        return 'filter_all';
+    }
+}
+
+function serviceKindReducer(state = 'set_service_all', action) {
+    switch (action.type) {
+    case 'set_service_all':
+        return 'service_all';
+    case 'set_service_google':
+        return 'service_google';
+    case 'set_service_papago':
+        return 'service_papago';
+    case 'set_service_kakao':
+        return 'service_kakao';
+    default:
+        return 'service_all';
+    }
+}
+
 export default combineReducers({
     showModalReducer,
+    srcLangReducer,
+    targetLangReducer,
+    serviceKindReducer,
+    mdFilterReducer,
 });
